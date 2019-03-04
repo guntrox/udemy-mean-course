b5f995d063c042a8ab6d4a76feb9fbdb5f18cca7
Olwyn Meek <ojmeek@yahoo.com>
2019-03-03 20:35:59 -0500

    added with rxjs

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 import { Post } from './post.model';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
   private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
 
   getPosts() {
     return [...this.posts];
   }
 
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
+
   addPost(mytitle: string, mycontent: string) {
-    const post: Post = {
-      title: mytitle,
-      content: mycontent
-    };
+    const post: Post = { title: mytitle, content: mycontent };
     this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
   }
 }
