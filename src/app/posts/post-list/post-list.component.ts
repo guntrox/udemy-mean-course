1e9bc5e3d815c06aea344edbb53a6006f6069497
Olwyn Meek <ojmeek@yahoo.com>
2019-03-03 19:39:23 -0500

    added ngFor

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -6,10 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent {
-  posts = [
-    { title: 'First Post', content: 'This is some content' },
-    { title: 'Second Post', content: 'This is some content' },
-    { title: 'Third Post', content: 'This is some content' },
-    { title: 'Fourth Post', content: 'This is some content' }
-  ];
+  // posts = [
+  //   { title: 'First Post', content: 'This is some content' },
+  //   { title: 'Second Post', content: 'This is some content' },
+  //   { title: 'Third Post', content: 'This is some content' },
+  //   { title: 'Fourth Post', content: 'This is some content' }
+  // ];
+  posts = [];
 }
