f90806c2de370b7be534286795c92e50b81383ec
Olwyn Meek <ojmeek@gmail.com>
2019-03-06 19:48:51 -0500

    added the mongo db cloud access

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+const Post = require('./models/post');
 
 const app = express();
+
+// db_password = SF8JDipPTMlSBfuN
+mongoose.connect("mongodb+srv://olwyn:SF8JDipPTMlSBfuN@udemy-mean-course-rbajx.mongodb.net/node-angular?retryWrites=true")
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch( () => {
+    console.log('Connection failed!');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -20,30 +33,35 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully'
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
   });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost._id
+    });
+  });
+
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'famkelcm',
-      title: 'First server-side post',
-      content: 'This is coming from the server'
-    },
-    {
-      id: 'fcdsaccm',
-      title: 'Second server-side post',
-      content: 'This is coming from the server'
-    }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents
+      });
+    });
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({_id: req.params.id}).then(createdPost => {
+    console.log(createdPost);
+    res.status(200).json({ message: "Post deleted!"});
   });
 });
+
 
 module.exports = app;
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
-import { post } from 'selenium-webdriver/http';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -13,9 +13,21 @@ export class PostsService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+    this.http
+      .get<{message: string, posts: any}>(
+        'http://localhost:3000/api/posts'
+      )
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          };
+        });
+      }))
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
@@ -26,10 +38,21 @@ export class PostsService {
 
   addPost(mytitle: string, mycontent: string) {
     const post: Post = { id: null, title: mytitle, content: mycontent };
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+    this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
       .subscribe((responseData) => {
         console.log(responseData.message);
+        const postId = responseData.postId;
+        post.id = postId;
         this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  deletePost(postId: string) {
+    this.http.delete('http://localhost:3000/api/posts/' + postId)
+      .subscribe(() => {
+        const updatedPosts = this.posts.filter(post => post.id !== postId);
+        this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
