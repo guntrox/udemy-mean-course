2b380616f184639c91c848752b91a7ccdf4a5b46
Olwyn Meek <ojmeek@gmail.com>
2019-03-07 18:13:52 -0500

    added routes/posts.js for backend, updated editing a post to reload properly

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
+import { stringify } from 'querystring';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -37,7 +38,8 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return {...this.posts.find(p => p.id === id)};
+    return this.http.get<{_id: string; title: string; content: string}>(
+      'http://localhost:3000/api/posts/' + id);
   }
 
   addPost(mytitle: string, mycontent: string) {
@@ -57,7 +59,13 @@ export class PostsService {
       id: id, title: title, content: content
     };
     this.http.put('http://localhost:3000/api/posts/' + id, post)
-      .subscribe(response => console.log(response));
+      .subscribe(() => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   deletePost(postId: string) {
